c6c6867afc50975e2fc15d639d7263ae0f141710
Nikita Ignatev <nikita22ignatev@outlook.com>
2023-03-11 15:59:48 +0300

    window style and controls

--- SoilClassificationWPF/MainWindow.xaml.cs
+++ SoilClassificationWPF/MainWindow.xaml.cs
@@ -15,14 +15,22 @@ using System.Windows.Shapes;
 
 namespace SoilClassificationWPF
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+        private void CloseAppClick(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
