f9f54d8193ba4ec3c5dbced902f559fdf98d1318
Nikita Ignatev <nikita22ignatev@outlook.com>
2023-03-17 00:16:36 +0300

    Git link

--- SoilClassificationWPF/Pages/MainPage.xaml.cs
+++ SoilClassificationWPF/Pages/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,11 @@ namespace SoilClassificationWPF
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void Git(object sender, RoutedEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo("https://github.com/nikita22ignatev/SoilClassificationWPF") { UseShellExecute = true });
         }
     }
 }
